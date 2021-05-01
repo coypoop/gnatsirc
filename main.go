2916444efe0cf1ea60bea33e77ed342bc8ea9c32
Maya Rashish <maya@NetBSD.org>
2021-05-01 16:09:07 +0300

    Use flag pkg, don't leak password in ident, support allowlist for categories

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -20,44 +21,65 @@ const (
 	PRStartScan = 55680
 )
 
-func main() {
-	credentials := false
-	ircPassword := "password"
+var (
+	allowedCategories categorySlice
+	ircPassword *string
+	ircChannel *string
+	ircServer *string
+	ircUsername *string
+)
 
+type categorySlice []string
+
+func (i *categorySlice) String() string {
+	return "my string representation"
+}
+
+func (i *categorySlice) Set(value string) error {
+	*i = append(*i, value)
+	return nil
+}
+
+func main() {
+	flag.Var(&allowedCategories, "allow-category", "Only post PRs from these categories.")
+	ircPassword = flag.String("irc-password", "irc-password", "Password to be used for ")
+	ircServer = flag.String("irc-server", "irc-server", "Which IRC server to connect, for example irc.example.com:6667")
+	ircChannel = flag.String("irc-channel", "irc-channel", "Which IRC channel to join, for example #my-channel")
+	ircUsername = flag.String("irc-username", "irc-username", "Which username to use on IRC")
+
+	flag.Parse()
+
+	if ircServer == nil ||
+	   ircChannel == nil ||
+	   ircUsername == nil {
+		   usage()
+	}
 	if len(os.Args) < 4 {
 		usage()
 	}
 
-	ircServer := os.Args[1]
-	ircChan := os.Args[2]
-	ircUsername := os.Args[3]
-	if len(os.Args) >= 5 {
-		credentials = true
-		ircPassword = os.Args[4]
-	}
-
 	config := irc.ClientConfig{
-		Nick: ircUsername,
-		Pass: ircUsername,
-		User: ircPassword,
+		Nick: *ircUsername,
+		Pass: *ircPassword,
+		User: *ircUsername,
 		Name: "GNATS urls on demand",
 		Handler: irc.HandlerFunc(func(c *irc.Client, m *irc.Message) {
 			fmt.Printf("%v\n", m)
 			if m.Command == "001" {
-				log.Printf("Connected to server %s", ircServer)
-				// 001 is a welcome event, so we join channels there
-				if credentials {
+				log.Printf("Connected to server %s", *ircServer)
+				// 001 is a welcome event, so we identify join channels now
+				if ircPassword != nil {
 					c.WriteMessage(&irc.Message{
 						Command: "PRIVMSG",
 						Params: []string{
 							"NickServ",
-							"IDENTIFY " + ircUsername + " " + ircPassword,
+							"IDENTIFY " + *ircUsername + " " + *ircPassword,
 						},
 					})
 				}
-				c.Write("JOIN " + ircChan)
-				log.Printf("Joined %s", ircChan)
-				go observeNewPRs(c, ircChan)
+				c.Write("JOIN " + *ircChannel)
+				log.Printf("Joined %s", *ircChannel)
+				go observeNewPRs(c, *ircChannel)
 			} else if m.Command == "PRIVMSG" && c.FromChannel(m) {
 				if selfMsg(m.Trailing()) {
 					return
@@ -89,7 +111,7 @@ func main() {
 
 
 	for {
-		conn, err := net.Dial("tcp", ircServer)
+		conn, err := net.Dial("tcp", *ircServer)
 		if err != nil {
 			time.Sleep(1 * time.Minute)
 			continue
@@ -112,6 +134,15 @@ func observeNewPRs(c *irc.Client, ircChan string) {
 		for currentPR := latestGoodPR; currentPR-latestGoodPR < 20; currentPR++ {
 			currentSynopsis, err := findPRSynopsis(fmt.Sprintf("https://gnats.netbsd.org/%d", currentPR))
 			if err != nil {
+				continue
+			}
+			currentCategory, err := findPRCategory(fmt.Sprintf("https://gnats.netbsd.org/%d", currentPR))
+			if err != nil {
+				continue
+			}
+			if !allowedCategory(currentCategory) {
+				fmt.Printf("Not posting new PR %d (%s), current category %s not in allowed categories %v\n",
+				    currentPR, currentSynopsis, currentCategory, allowedCategories)
 				continue
 			}
 			latestGoodPR = currentPR
@@ -145,6 +176,20 @@ func observeNewPRs(c *irc.Client, ircChan string) {
 	}
 }
 
+func allowedCategory(testedCategory string) bool {
+	if allowedCategories == nil {
+		return true
+	}
+
+	for _, allowedCategory := range(allowedCategories) {
+		if testedCategory == allowedCategory {
+			return true
+		}
+	}
+
+	return false
+}
+
 func toGnatsUrl(prNum int) string {
 	return fmt.Sprintf("https://gnats.netbsd.org/%d", prNum)
 }
@@ -156,6 +201,25 @@ func selfMsg(msg string) bool {
 		return true
 	}
 	return false
+}
+
+func findPRCategory(prUrl string) (string, error) {
+	resp, err := http.Get(prUrl)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	rs := categoryRegexp.FindSubmatch(body)
+	if len(rs) > 1 {
+		prCategoryHtmlSanitized := rs[1]
+		prCategory := undoHtmlSanitize(string(prCategoryHtmlSanitized))
+		return prCategory, nil
+	}
+	return "", errors.New("Not found category in body")
 }
 
 func findPRSynopsis(prUrl string) (string, error) {
@@ -203,11 +267,13 @@ func undoHtmlSanitize(msg string) string {
 
 var prRegexps []*regexp.Regexp
 var synopsisRegexp *regexp.Regexp
+var categoryRegexp *regexp.Regexp
 var selfMsgRegexp *regexp.Regexp
 
 func init() {
 	selfMsgRegexp = regexp.MustCompile(`https://gnats.netbsd.org`)
 	synopsisRegexp = regexp.MustCompile(`.*Synopsis:.... *(.*)`)
+	categoryRegexp = regexp.MustCompile(`.*Category:.... *(.*)`)
 	prRegexps = []*regexp.Regexp{
 		regexp.MustCompile("PR [a-z]*/([0-9]{4,5})"),
 		regexp.MustCompile("PR ([0-9]{4,5})"),
@@ -217,6 +283,7 @@ func init() {
 }
 
 func usage() {
-	fmt.Printf("Usage:\t%s irc.example.com:6667 #netbsd username [password]\n", os.Args[0])
+	fmt.Printf("Usage:\t%s -irc-server irc.example.com:6667 -irc-channel #netbsd -irc-username username [-irc-password password] [-allow-category pkg]\n", os.Args[0])
+	flag.PrintDefaults()
 	os.Exit(1)
 }
